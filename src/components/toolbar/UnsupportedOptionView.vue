931a440f2e84a0690107d2195f63f072e39ad59f
Nike Okoronkwo <nikechukwu@gmail.com>
2025-06-04 21:42:45 -0400

    Updated Unsupported Option

--- src/components/toolbar/UnsupportedOptionView.vue
+++ src/components/toolbar/UnsupportedOptionView.vue
@@ -7,10 +7,13 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center h-full">
+  <div class="flex flex-col justify-center items-center h-full space-y-2 text-center w-full">
     <Icon icon="mage:robot-sad" class="scale-[6] pb-1" />
     <div class=" text-2xl font-bold">Whoops</div>
-    <p>This feature is not supported yet. Sorry for the inconvenience</p>
+    <p class="flex flex-col justify-center items-center text-center">
+      <span>This feature is not supported yet.</span>
+      <span>Sorry for the inconvenience</span>
+    </p>
     <p v-if="coming">
       We are currently working on it: you can take a look <a :href="coming" target="_blank">here</a>
     </p>
